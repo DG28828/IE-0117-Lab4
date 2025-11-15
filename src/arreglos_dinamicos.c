020eb94400410da1a8d54a58ca2fab420272e53f
DG28828 <dannygarro28801@gmail.com>
2025-11-15 08:24:16 -0600

    Agregado implementacion de fucion array_create para crear arreglos dinámicos

--- src/arreglos_dinamicos.c
+++ src/arreglos_dinamicos.c
@@ -1,1 +1,12 @@
 #include "arreglos_dinamicos.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int *array_create(int size) {
+    int *array = (int *)malloc(size*sizeof(int));
+    if (array == NULL) {        // ¿Lo mismo si se pone if (!array)?
+	printf("Error: No se pudo asignar memoria para el array. \n");
+	return NULL;
+    }
+    return array;
+}
